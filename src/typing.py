d5e9503f85b3edd6377a1cc9f0a2c19b0874deb8
Myounghoon Lee <lmhchris47@snu.ac.kr>
2021-07-23 01:26:35 +0900

    Add Portfolio type

---
+++ src/typing.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Portfolio:
+    celltrion: int
+    hyundai_motors: int
+    kakao: int
+    kospi: int
+    lg_chem: int
+    lg_hnh: int
+    naver: int
+    samsung_bio: int
+    samsung_elec: int
+    samsung_elec2: int
+    samsung_sdi: int
+    sk_hynix: int
+
+    capital: int
