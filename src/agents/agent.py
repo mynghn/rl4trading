04d704e89d999f016a253aac872cd120ced6061a
Myounghoon Lee <lmhchris47@snu.ac.kr>
2021-07-23 03:59:18 +0900

    Add sell & buy abstract methods

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -11,3 +11,11 @@ class Agent(ABC):
     @abstractmethod
     def action(self, state: Sequence[Union[int, float]], **kwargs) -> Union[int, float]:
         raise NotImplementedError
+
+    @abstractmethod
+    def sell(self, stock: int, quantity: int, price: int, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def buy(self, stock: int, quantity: int, price: int, **kwargs):
+        raise NotImplementedError
