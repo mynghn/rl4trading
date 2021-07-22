5bdff631f10a3d13793966ad7fbaff829e57af7c
Myounghoon Lee <lmhchris47@snu.ac.kr>
2021-07-23 01:50:46 +0900

    Set instance portfolio without passing param

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -5,8 +5,8 @@ from typings import Portfolio
 
 
 class Agent(ABC):
-    def __init__(self, portfolio: Portfolio):
-        self.portfolio = portfolio
+    def __init__(self):
+        self.portfolio = Portfolio()
 
     @abstractmethod
     def action(self, state: Sequence[Union[int, float]], **kwargs) -> Union[int, float]:
